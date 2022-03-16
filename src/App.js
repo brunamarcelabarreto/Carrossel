74f7108591714cfc5b2fb6ece3ec27c07c3c8a52
brunamarcelabarreto <bruna_marcelabarreto@hotmail.com>
2022-03-15 22:31:35 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src="/static/images/super-shoes.png" alt="Super Shoes Logo" />
+      </div>
+      <div className="carrossel">
+        <div class="item">
+          <div className="image">
+            <img src="https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg" alt="Shoe" />
+          </div>
+          <div className="info">
+            <span className="name">Super Shoe 1</span>
+            <span className="oldPrice">U$ 299.00</span>
+            <span className="price">U$ 199.00</span>
+          </div>
+       </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
